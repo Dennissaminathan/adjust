5b6082a5c2627e71003f1aa1ade1320521db6142
Dennissaminathan <36468171+Dennissaminathan@users.noreply.github.com>
2021-04-24 23:10:25 +0530

    Updated script with some useful comments

--- random_number.py
+++ random_number.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 import random
 
-numbers =[1,2,3,4,5,6,7,8,9,10]
-def myfunction():
+numbers =[1,2,3,4,5,6,7,8,9,10]  ### List with required numbers
+def myfunction():    ### Function to pass floating variable for the shuffle method
   return 0.1
 
 def print_random_order():
     random.shuffle(numbers,myfunction)
-    for random_number in numbers:
+    for random_number in numbers:  ### Print the randomly ordered numbers in the list to the terminal
         print random_number
 
-print_random_order()
-
+print_random_order() ### Call to the print_random_order method.
